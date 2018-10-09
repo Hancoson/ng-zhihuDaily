c7d0aa3860fbe94e5d8685affa771df281750355
hanguoxing <11090096@bbktel.com>
2018-10-09 10:53:41 +0800

    ci: add commitizen

--- src/app/components/detail/detail.component.spec.ts
+++ src/app/components/detail/detail.component.spec.ts
@@ -8,9 +8,9 @@ describe('DetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
+      declarations: [DetailComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
